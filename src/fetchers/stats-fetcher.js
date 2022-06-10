7ed7f0330a06eee278292a491bf688dbcb3e9067
apt9 <81689524+apt-9@users.noreply.github.com>
2022-06-10 14:38:27 +0200

    Update stats-fetcher.js

--- src/fetchers/stats-fetcher.js
+++ src/fetchers/stats-fetcher.js
@@ -63,8 +63,8 @@ const fetcher = (variables, token) => {
   );
 };
 
-// https://github.com/anuraghazra/github-readme-stats/issues/92#issuecomment-661026467
-// https://github.com/anuraghazra/github-readme-stats/pull/211/
+// https://github.com/apt-9/github-readme-stats/issues/92#issuecomment-661026467
+// https://github.com/apt-9/github-readme-stats/pull/211/
 const totalCommitsFetcher = async (username) => {
   if (!githubUsernameRegex.test(username)) {
     logger.log("Invalid username");
